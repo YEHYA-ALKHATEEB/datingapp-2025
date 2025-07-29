b8cbc6e12e538d0b35f39566a4792896a3a796c0
yehya alkhateeb <yehya.alkhateeb@gmail.com>
2025-07-29 16:01:09 +0300

    Use Angular HttpClient to fetch members list from API

---
+++ client/src/app/app.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  protected readonly title = signal('Dating App');
+protected members = signal<any>([]);
+
+  async ngOnInit(): Promise<void> {
+    // Initialization logic can go here
+    this.members.set(await this.getMembers());
+
+    // Example HTTP request (uncomment to use)
+  //   this.http.get('https://localhost:7046/api/members').subscribe({
+  //     next: Response => this.members.set(Response),
+  //     error: error => console.log(error),
+  //     complete: () => console.log('completed the http request')
+  // })
+}
+  
+  // Example method to fetch members
+
+async getMembers() {
+
+try {
+    return lastValueFrom(this.http.get('https://localhost:7046/api/members'));
+
+} catch (error) {
+  console.log(error);
+  throw error;
+}
+
+
+
+}
+}
