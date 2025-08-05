2f26b6681cb73090e1eaeb8b420ec6fda2d662c7
yehya alkhateeb <yehya.alkhateeb@gmail.com>
2025-08-05 13:41:12 +0300

    feat: create angular service tologin

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../core/services/account-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
+  private accountService = inject(AccountService);
+  protected credentials: any = {}
+  protected loggedIn = signal(false);
 
+  login() {
+    this.accountService.login(this.credentials).subscribe({
+      next: response => {
+        console.log('Login successful', response);
+        this.loggedIn.set(true);
+      },
+      error: error => alert(error.message),
+      complete: () => {
+        console.log('Login request completed');
+      }
+    })
+  }
+
+  logout() {
+    this.loggedIn.set(false);
+    console.log('Logged out');
+  }
 }
