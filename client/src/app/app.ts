3c11065b85e4ab780e2f8378273c53ada6a05016
yehya alkhateeb <yehya.alkhateeb@gmail.com>
2025-08-08 22:25:36 +0300

    feat: Adding route animations

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,9 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { Component, inject } from '@angular/core';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { User } from '../types/user';
 import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -12,47 +8,7 @@ import { Router, RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  private accountService = inject(AccountService);
+export class App {
   protected router = inject(Router);
-  private http = inject(HttpClient);
-protected  title = signal('Dating App');
-protected members = signal<User[]>([]);
 
-  async ngOnInit(): Promise<void> {
-    // Initialization logic can go here
-    this.members.set(await this.getMembers());
- this.setCurrentUser();
-    // Example HTTP request (uncomment to use)
-  //   this.http.get('https://localhost:7046/api/members').subscribe({
-  //     next: Response => this.members.set(Response),
-  //     error: error => console.log(error),
-  //     complete: () => console.log('completed the http request')
-  // })
-}
-
-setCurrentUser() {
-  const userString = localStorage.getItem('user');
-  if (!userString) {
-    return
-  }
-  const user = JSON.parse(userString);
-  this.accountService.currentUser.set(user);
-}
-  
-  // Example method to fetch members
-
-async getMembers() {
-
-try {
-    return lastValueFrom(this.http.get<User[]>('https://localhost:7046/api/members'));
-
-} catch (error) {
-  console.log(error);
-  throw error;
-}
-
-
-
-}
 }
