6431402d3d1070f8ec757ad11bebe341eb4fbcaf
yehya alkhateeb <yehya.alkhateeb@gmail.com>
2025-08-20 00:22:23 +0300

    feat: Extending the User entity

--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -5,4 +5,6 @@ using Microsoft.EntityFrameworkCore;
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<AppUser> Users { get; set; }
+    public DbSet<Member> Members { get; set; }
+    public DbSet<Photo> Photos { get; set; }
 }
